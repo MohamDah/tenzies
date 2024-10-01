c1650a0631d7eade55a6722a16cb3d55b386a582
MohamDah <mohammedfaiseldhb@gmail.com>
2024-10-01 08:53:19 +0200

    added link to repo

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,11 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <p class="credit">Coded by <a target="_blank" href="https://github.com/MohamDah">Mohamed Dahab</a>
+      <small>
+        Check the <a target="_blank" href="https://github.com/MohamDah/tenzies">
+          source code</a>
+      </small>
+    </p>
   </StrictMode>,
 )
